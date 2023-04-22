8013d3c63559101ef3f030f5092dcedd9b7cff43
Mason, Wu <mason.emulator@gmail.com>
2023-04-22 13:56:04 +0800

    modify home screen, add Carousel

--- app/src/main/java/tw/mason/mcdonalds/screen/HomeScreen.kt
+++ app/src/main/java/tw/mason/mcdonalds/screen/HomeScreen.kt
@@ -1,7 +1,9 @@
 package tw.mason.mcdonalds.screen
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.collectIsDraggedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,14 +11,29 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -24,10 +41,16 @@ import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import kotlinx.coroutines.delay
 import tw.mason.mcdonalds.R
 import tw.mason.mcdonalds.ui.theme.YELLOW
 
@@ -98,9 +121,126 @@ fun HomeScreen() {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp)
+                .padding(horizontal = 16.dp)
         ) {
-            Text(text = "晚安!", fontSize = 32.sp)
+            Text(
+                text = "晚安!",
+                fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            McCarousel()
         }
     }
+}
+
+
+@Composable
+fun McCarousel() {
+    val images = listOf(
+        "https://www.mcdonalds.com/content/dam/sites/tw/carousel/2023/Carousel_0419Signature_2336x1040.png",
+        "https://www.mcdonalds.com/content/dam/sites/tw/carousel/2023/Colombia0411%E5%A4%A7%E9%A6%96%E9%A0%81%E8%BC%AA%E6%92%AD_2336x1040.png",
+    )
+
+    Card(
+        modifier = Modifier,
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        @OptIn(ExperimentalFoundationApi::class)
+        AutoSlidingCarousel(itemsCount = images.size) { index ->
+            AsyncImage(
+                model = ImageRequest
+                    .Builder(LocalContext.current)
+                    .data(images[index])
+                    .crossfade(true)
+                    .build(),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.height(140.dp)
+            )
+        }
+    }
+}
+
+const val AUTO_SLIDE_DURATION = 3000L
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun AutoSlidingCarousel(
+    modifier: Modifier = Modifier,
+    autoSlideDuration: Long = AUTO_SLIDE_DURATION,
+    pagerState: PagerState = rememberPagerState(),
+    itemsCount: Int,
+    itemContent: @Composable (index: Int) -> Unit
+) {
+    val isDragged by pagerState.interactionSource.collectIsDraggedAsState()
+    var key by remember { mutableStateOf(false) }
+
+    LaunchedEffect(key1 = key) {
+        delay(autoSlideDuration)
+        val target = if (pagerState.currentPage < itemsCount - 1) pagerState.currentPage + 1 else 0
+        pagerState.animateScrollToPage(page = target)
+        key = !key
+    }
+
+    Box(modifier = modifier.fillMaxWidth()) {
+        HorizontalPager(pageCount = itemsCount, state = pagerState) { index ->
+            itemContent(index)
+        }
+
+        Surface(
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .align(Alignment.BottomCenter),
+            shape = CircleShape,
+            color = Color.Transparent
+        ) {
+            DotsIndicator(
+                modifier = Modifier.padding(horizontal = 8.dp, vertical = 6.dp),
+                totalDots = itemsCount,
+                selectedIndex = if (isDragged) pagerState.currentPage else pagerState.targetPage,
+                dotSize = 8.dp
+            )
+        }
+    }
+}
+
+@Composable
+fun DotsIndicator(
+    modifier: Modifier = Modifier,
+    totalDots: Int,
+    selectedIndex: Int,
+    selectedColor: Color = YELLOW,
+    unSelectedColor: Color = Color.White,
+    dotSize: Dp
+) {
+    LazyRow(
+        modifier = modifier
+            .wrapContentWidth()
+            .wrapContentHeight()
+    ) {
+        items(totalDots) { index ->
+            IndicatorDot(
+                size = dotSize,
+                color = if (index == selectedIndex) selectedColor else unSelectedColor
+            )
+            if (index != totalDots - 1) {
+                Spacer(modifier = Modifier.padding(horizontal = 4.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun IndicatorDot(
+    modifier: Modifier = Modifier,
+    size: Dp,
+    color: Color
+) {
+    Box(
+        modifier = modifier
+            .size(size)
+            .clip(CircleShape)
+            .background(color)
+    )
 }
