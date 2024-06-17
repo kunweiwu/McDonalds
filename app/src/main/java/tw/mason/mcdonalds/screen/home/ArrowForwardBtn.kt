249d6b7c317cfa7fa935165d8133359f61d9bce5
Mason, Wu <S105000169@g.ksu.edu.tw>
2024-06-17 17:02:36 +0800

    Update Kotlin to 1.9.24

--- app/src/main/java/tw/mason/mcdonalds/screen/home/ArrowForwardBtn.kt
+++ app/src/main/java/tw/mason/mcdonalds/screen/home/ArrowForwardBtn.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
@@ -23,7 +23,7 @@ fun ArrowForwardBtn(
             .size(30.dp)
     ) {
         Icon(
-            imageVector = Icons.Default.ArrowForward,
+            imageVector = Icons.AutoMirrored.Filled.ArrowForward,
             contentDescription = null,
             tint = Color(0xFF666666),
             modifier = Modifier.size(16.dp)
